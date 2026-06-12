355193cfb1b11103e51a2bcb96edd26da8ff9499
Seth Tenenbaum <sethtenenbaum@protonmail.com>
2026-06-12 03:11:46 -0400

    Extract VST3 process context helper

--- native/bridge-daemon/src/Vst3HostWorker.cpp
+++ native/bridge-daemon/src/Vst3HostWorker.cpp
@@ -4,6 +4,7 @@
 #include "SoundBridge/ExampleInstrumentRenderer.h"
 #include "SoundBridge/NativePlugin.h"
 #include "SoundBridge/NativeFileGrantSupport.h"
+#include "SoundBridge/Vst3HostWorkerProcessSupport.h"
 #include "SoundBridge/Vst3HostWorkerSupport.h"
 
 #ifdef SOUNDBRIDGE_ENABLE_VST3_SDK
@@ -183,42 +184,7 @@ public:
     processData.inputs = inputBusBuffers.empty() ? nullptr : inputBusBuffers.data();
     processData.outputs = outputBusBuffers.empty() ? nullptr : outputBusBuffers.data();
 
-    Steinberg::Vst::ProcessContext processContext {};
-    processContext.sampleRate = sampleRate_;
-    processContext.projectTimeSamples = transport.samplePosition;
-    processContext.continousTimeSamples = transport.samplePosition;
-    processContext.state = Steinberg::Vst::ProcessContext::kContTimeValid;
-    if (transport.playing) {
-      processContext.state |= Steinberg::Vst::ProcessContext::kPlaying;
-    }
-    if (transport.recording) {
-      processContext.state |= Steinberg::Vst::ProcessContext::kRecording;
-    }
-    if (transport.loopActive) {
-      processContext.state |= Steinberg::Vst::ProcessContext::kCycleActive;
-    }
-    if (transport.hasTempo) {
-      processContext.tempo = transport.tempo;
-      processContext.state |= Steinberg::Vst::ProcessContext::kTempoValid;
-    }
-    if (transport.hasTimeSignature) {
-      processContext.timeSigNumerator = transport.timeSignatureNumerator;
-      processContext.timeSigDenominator = transport.timeSignatureDenominator;
-      processContext.state |= Steinberg::Vst::ProcessContext::kTimeSigValid;
-    }
-    if (transport.hasProjectTimeMusic) {
-      processContext.projectTimeMusic = transport.projectTimeMusic;
-      processContext.state |= Steinberg::Vst::ProcessContext::kProjectTimeMusicValid;
-    }
-    if (transport.hasBarPositionMusic) {
-      processContext.barPositionMusic = transport.barPositionMusic;
-      processContext.state |= Steinberg::Vst::ProcessContext::kBarPositionValid;
-    }
-    if (transport.hasCycle) {
-      processContext.cycleStartMusic = transport.cycleStartMusic;
-      processContext.cycleEndMusic = transport.cycleEndMusic;
-      processContext.state |= Steinberg::Vst::ProcessContext::kCycleValid;
-    }
+    auto processContext = processContextForTransport(transport, sampleRate_);
     processData.processContext = &processContext;
 
     auto midiEvents = std::move(pendingMidiEvents_);
---
+++ native/bridge-daemon/src/Vst3HostWorkerProcessSupport.cpp
@@ -0,0 +1,53 @@
+#include "SoundBridge/Vst3HostWorkerProcessSupport.h"
+
+#ifdef SOUNDBRIDGE_ENABLE_VST3_SDK
+
+#include "SoundBridge/Vst3HostWorkerSupport.h"
+
+namespace soundbridge::vst3_worker {
+
+Steinberg::Vst::ProcessContext processContextForTransport(
+    const HostTransportContext& transport,
+    double sampleRate) {
+  Steinberg::Vst::ProcessContext processContext {};
+  processContext.sampleRate = sampleRate;
+  processContext.projectTimeSamples = transport.samplePosition;
+  processContext.continousTimeSamples = transport.samplePosition;
+  processContext.state = Steinberg::Vst::ProcessContext::kContTimeValid;
+  if (transport.playing) {
+    processContext.state |= Steinberg::Vst::ProcessContext::kPlaying;
+  }
+  if (transport.recording) {
+    processContext.state |= Steinberg::Vst::ProcessContext::kRecording;
+  }
+  if (transport.loopActive) {
+    processContext.state |= Steinberg::Vst::ProcessContext::kCycleActive;
+  }
+  if (transport.hasTempo) {
+    processContext.tempo = transport.tempo;
+    processContext.state |= Steinberg::Vst::ProcessContext::kTempoValid;
+  }
+  if (transport.hasTimeSignature) {
+    processContext.timeSigNumerator = transport.timeSignatureNumerator;
+    processContext.timeSigDenominator = transport.timeSignatureDenominator;
+    processContext.state |= Steinberg::Vst::ProcessContext::kTimeSigValid;
+  }
+  if (transport.hasProjectTimeMusic) {
+    processContext.projectTimeMusic = transport.projectTimeMusic;
+    processContext.state |= Steinberg::Vst::ProcessContext::kProjectTimeMusicValid;
+  }
+  if (transport.hasBarPositionMusic) {
+    processContext.barPositionMusic = transport.barPositionMusic;
+    processContext.state |= Steinberg::Vst::ProcessContext::kBarPositionValid;
+  }
+  if (transport.hasCycle) {
+    processContext.cycleStartMusic = transport.cycleStartMusic;
+    processContext.cycleEndMusic = transport.cycleEndMusic;
+    processContext.state |= Steinberg::Vst::ProcessContext::kCycleValid;
+  }
+  return processContext;
+}
+
+} // namespace soundbridge::vst3_worker
+
+#endif
